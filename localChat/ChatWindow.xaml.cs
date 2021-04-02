562c568c5d316feaa5a67ab3f75e236e1b73fbbe
Kirill <60620490+KirillGubich@users.noreply.github.com>
2021-04-02 19:58:06 +0300

    GUI implementation

---
+++ localChat/ChatWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace localChat
+{
+    /// <summary>
+    /// Логика взаимодействия для ChatWindow.xaml
+    /// </summary>
+    public partial class ChatWindow : Window
+    {
+        private ChatManager chatManager;
+        public ChatWindow()
+        {
+            InitializeComponent();
+        }
+
+        public ChatWindow(string name)
+        {
+            InitializeComponent();
+            chatManager = new ChatManager(name);
+            chatManager.initialize();
+        }
+
+        private void btnSendClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
